e6f4ff0655f5b0e44620cab5af318673d45c7eec
mantzoutec <mantzoutec@gmail.com>
2021-03-28 11:32:49 +0300

    test

---
+++ anakoinoseis.php
@@ -0,0 +1,118 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Header -->
+<?php include 'head.php'; ?>
+
+<body>
+    <!-- Header & Navigation -->
+    <?php include 'header_nav.php'; ?>
+
+    <!-- Body -->
+    <div class="container-fluid">
+        <br>
+        <h6>
+            <strong>ΑΝΑΚΟΙΝΩΣΕΙΣ ΕΑΠ</strong>
+        </h6>
+        <br>
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="photos/anakoinosi1.jpg" class="card-img-top" alt="Radio EAP">
+                        <div class="card-body">
+                            <h5 class="card-title">Radio ΕΑΠ</h5>
+                            <p class="card-text">Μια ακόμη διάκριση «συνοδεύει» εδώ και μερικές ημέρες την ομάδα
+                                παραγωγών του Ραδιοφωνικού Σταθμού.</p>
+                            <a href="anakoinosi1.php" class="btn btn-primary">Δες περισσότερα</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="photos/anakoinosi2.jpg" class="card-img-top" alt="Λίστα αναγνώρισης διδασκόντων ΕΑΠ">
+                        <div class="card-body">
+                            <h5 class="card-title">Λίστα αναγνώρισης διδασκόντων ΕΑΠ</h5>
+                            <p class="card-text">Το Ελληνικό Ανοικτό Πανεπιστήμιο χαιρετίζει την αναγνώριση του υψηλού
+                                επιστημονικού επιπέδου των διδασκόντων του.</p>
+                            <a href="anakoinosi2.php" class="btn btn-primary">Δες περισσότερα</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="photos/anakoinosi3.jpg" class="card-img-top" alt="Διακοπή Χριστουγέννων">
+                        <div class="card-body">
+                            <h5 class="card-title">Διακοπή Χριστουγέννων</h5>
+                            <p class="card-text">
+                                Σας ενημερώνουμε ότι, κατόπιν απόφασης της Διοίκησης, το Ίδρυμά μας θα παραμείνει
+                                κλειστό από την Πέμπτη 24 Δεκεμβρίου ως την Κυριακή 3 Ιανουαρίου.
+                            </p>
+                            <a href="anakoinosi3.php" class="btn btn-primary">Δες περισσότερα</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="photos/anakoinosi2.jpg" class="card-img-top" alt="Υποτροφίες της Αυστριακής κυβέρνησης.">
+                        <div class="card-body">
+                            <h5 class="card-title">Υποτροφίες της Αυστριακής κυβέρνησης.</h5>
+                            <p class="card-text">Η Αυστριακή κυβέρνηση χορηγεί υποτροφίες σε μη αυστριακούς φοιτητές για
+                                το ακαδημαϊκό έτος 2021-2022.</p>
+                            <a href="anakoinosi4.php" class="btn btn-primary">Δες περισσότερα</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="photos/anakoinosi2.jpg" class="card-img-top" alt="Έναρξη Έκτου Κύκλου Σπουδών τριών Σύντομων Προγραμμάτων Σπουδών της ΣΘΕΤ">
+                        <div class="card-body">
+                            <h5 class="card-title">Έναρξη Έκτου Κύκλου Σπουδών τριών Σύντομων Προγραμμάτων Σπουδών της
+                                ΣΘΕΤ</h5>
+                            <p class="card-text">
+                                Η Σχολή Θετικών Επιστημών και Τεχνολογίας του Ελληνικού Ανοικτού Πανεπιστημίου ξεκινά
+                                την υποβολή αιτήσεων για τον 6ο κύκλο Σπουδών,
+                                ο οποίος θα ξεκινήσει τον Μάρτιο του 2020 για τα εξής Σύντομα Προγράμματα:
+                            </p>
+                            <a href="anakoinosi5.php" class="btn btn-primary">Δες περισσότερα</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="photos/anakoinosi2.jpg" class="card-img-top" alt="Ένα εγχείρημα που ξεκίνησε και δεν πρέπει να σταματήσει – Άρθρο στην Εφημερίδα των Συντακτών">
+                        <div class="card-body">
+                            <h5 class="card-title">Ένα εγχείρημα που ξεκίνησε και δεν πρέπει να σταματήσει – Άρθρο στην
+                                Εφημερίδα των Συντακτών</h5>
+                            <p class="card-text">
+                                Οι εκδόσεις από πέρσι έχουν πληθύνει, οι μεταφράσεις αυξηθεί
+                                με τον σκοπό να διατηρείται πάντα ο αρχικός στόχος με βιβλία που μπορούν να σταθούν ως
+                                εξαιρετική βιβλιογραφία για φοιτητές και ερευνητές.
+                            </p>
+                            <a href="anakoinosi6.php" class="btn btn-primary">Δες περισσότερα</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    </div>
+
+    <!-- Footer -->
+    <?php include 'footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+
+</html>
